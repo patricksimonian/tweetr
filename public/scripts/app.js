8e11eddcb9672cc93feb779ff67a99997a2c012a
Patrick Simonian <patricksimonian@gmail.com>
2016-10-19 23:01:57 +0000

    cleaned up code and added comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-
+  //composition field
+  var newtweetForm = $('.counter').closest('form');
   //initial load of tweets in database
   (function loadPage() {
     loadTweets(true);
   })();
-
+  //encodes the form data to remove  cross server scripting
   function encodeText(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   }
-
+  //validates the new tweet form text area,
+  //if there are 0  or more then 140 characters respectively,
+  //the .warning div element is modified to display the appropriate paragraph element for
+  //either false validation
   function validateText(str) {
     str = str.trim();
       if(str.length === 0) {
@@ -26,6 +30,9 @@ $(document).ready(function () {
           return true;
       }
   }
+  //if form data is invalid (see above) upon submission, this function will be called
+  //instead of the ajax post request
+  //the warning displays for 1.5 seconds
   function displayWarning() {
     $(".warning").css("display", "block");
     setTimeout(function() {
@@ -35,6 +42,8 @@ $(document).ready(function () {
     }, 1500);
 
   }
+  //creates a nested tweet element with the structure of:
+  // section[class=tweet]>(header>img+h2+span)+(article>p)+(footer>span)
   function createTweetElement(userObj) {
     var $tweet = $('<section></section>').addClass('tweet');
     var timeElapsed = Math.floor((Date.now() - userObj.created_at)/8.64e+7) //milliseconds in a day;
@@ -49,11 +58,12 @@ $(document).ready(function () {
 
     return $tweet;
   }
-
+  //renders a new tweet by passing in the appropriate data into the createTweetElement function
+  //on page load this function accepts an Array of objects from tweets.json and is thereign itterated through
+  //and appended to the tweets container, beyond page load only a single object from the last index of tweets.json
+  //is rendered
   function  renderTweet(tweets) {
     var $tweet = null;
-  //will porbably duplicate results . need to find a way of validating
-  //appends so they dont repeat if that  user/timestamp is already there
     if(Array.isArray(tweets)) {
       tweets.forEach(function(obj) {
         $tweet = createTweetElement(obj);
@@ -86,6 +96,40 @@ $(document).ready(function () {
     newtweetForm.children('.counter').text(0);
     checkTweetCounter(newtweetForm.children('textarea'));
   }
+
+  //toggle tweet field
+  $('#compose').on("click", function() {
+    checkTweetCounter(newtweetForm.children('textarea'));
+    newtweetForm.closest('section').slideToggle("medium");
+    newtweetForm.children('textarea').focus();
+   });
+ //need to redirect to composer form after
+
+  //update tweet char count
+  newtweetForm.children('textarea').keyup(function(event) {
+    checkTweetCounter(this);
+  });
+
+  //clear tweet message field
+  newtweetForm.children('.clear').on("click", function() {
+    clearField()
+  });
+ //post tweet field to server
+  $(".new-tweet > form").on("submit", function(event) {
+    var textarea = $(this).children("textarea");
+    textarea.val(encodeText(textarea.val()));
+    event.preventDefault();
+    if(validateText(textarea.val())) {
+      $.ajax({
+        url: "/tweets/",
+        method: "POST",
+        data: $(this).serialize()
+      }).done(loadTweets(false));
+      clearField();
+    } else {
+      displayWarning();
+    }
+  });
 ////////////////////////////////////////////////////////////////////////////////
   //need to make this modular.. ie pass in the ame string into a function that
   //changes css from a themes object for each name
@@ -113,63 +157,5 @@ $(document).ready(function () {
 
   });
 ////////////////////////////////////////////////////////////////////////////
-  //composition field
-  var newtweetForm = $('.counter').closest('form');
-
-  //toggle tweet field
-  ($('#compose')).on("click", function() {
-    checkTweetCounter(newtweetForm.children('textarea'));
-    newtweetForm.closest('section').slideToggle("medium");
-    newtweetForm.children('textarea').focus();
-   });
- //need to redirect to composer form after
-  //update tweet char count
-  newtweetForm.children('textarea').keyup(function(event) { checkTweetCounter(this); });
-
-  //clear tweet message field
-
-  newtweetForm.children('.clear').on("click", function() {
-    clearField()
-  });
- //post tweet field to server
-  $(".new-tweet > form").on("submit", function(event) {
-    var textarea = $(this).children("textarea");
-    textarea.val(encodeText(textarea.val()));
-    event.preventDefault();
-    if(validateText(textarea.val())) {
-      $.ajax({
-        url: "/tweets/",
-        method: "POST",
-        data: $(this).serialize()
-      }).done(loadTweets(false));
-      clearField();
-    } else {
-      displayWarning();
-    }
-  });
-
-
 
 });
-
-//reset text box
-
-
-
-/////////////////////////////
-//themes
-/*
-body background: #eee
-
-Head:
-
-nav bar background: #009f86
-color: #e8fdff
-
-
-
-
-
-
-
-*/
